4a85bfa68ebe91b4c42eb63173267a870219ecd0
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-03-03 09:45:12 +0530

    Show remaining time, nuke index

--- index.php
+++ index.php
@@ -7,12 +7,41 @@
   <script>
     var colorMap = ["#00ff40", "#00ff00", "#40ff00", "#80ff00", "#bfff00", "#ffff00", "#ffbf00", "#ff8000", "#ff4000", "#ff0000"];
 
+    var set_time_left = function(cell, date_future) {
+        setInterval(function() {
+            var delta = Math.abs(date_future - (Date.now() / 1000));
+            var days = Math.floor(delta / 86400);
+            delta -= days * 86400;
+            var hours = Math.floor(delta / 3600) % 24;
+            delta -= hours * 3600;
+            var minutes = Math.floor(delta / 60) % 60;
+            delta -= minutes * 60;
+            var seconds = Math.floor(delta % 60);
+            cell.text(days + "d " + hours + "h " + minutes + "m " + seconds + "s");
+        }, 1000);
+    }
+
+    var set_time_spent = function(progress_bar, valid_from, valid_to) {
+        setInterval(function() {
+            var total_time = valid_to - valid_from;
+            var time_spent = Math.floor(Date.now() / 1000) - valid_from;
+            var percentage = Math.floor((time_spent * 100) / total_time);
+            percentage = percentage > 100 ? 100 : percentage;
+            progress_bar.text(percentage + "%");
+            progress_bar.css("color", "#000000");
+            progress_bar.css("width", percentage + "%");
+            progress_bar.attr("aria-valuenow", percentage);
+            progress_bar.css("background-color", colorMap[Math.floor(percentage / 10)]);
+        }, 1000);
+    }
+
     $(document).ready(function() {
         $(".dom-btn").click(function() {
 	    var row = $(this).closest("tr");
             var fqdn = row.find("td.fqdn").text();
             var port = row.find("td.port").text();
             var issuer = row.find("td.issuer");
+            var time_left = row.find("td.timeleft");
             var loading = row.find("img.loading");
 	    var progress = row.find("div.progress");
 	    var progress_bar = row.find("div.progress-bar");
@@ -28,16 +57,9 @@
                     loading.hide();
                     result = $.parseJSON(reply.responseText)
                     issuer.text(result["issuer"]["CN"]);
-                    var total_time = result['validTo_time_t'] - result['validFrom_time_t'];
-                    var time_spent = Math.floor(Date.now() / 1000) - result['validFrom_time_t'];
-                    var percentage = Math.floor((time_spent * 100) / total_time);
-		    percentage = percentage > 100 ? 100 : percentage;
-		    progress_bar.text(percentage + "%");
-	            progress_bar.css("color", "#000000");
-	            progress_bar.css("width", percentage + "%");
-		    progress_bar.attr("aria-valuenow", percentage);
-	            progress_bar.css("background-color", colorMap[Math.floor(percentage / 10)]);
-		    progress.show();
+                    set_time_left(time_left, result['validTo_time_t']);
+                    set_time_spent(progress_bar, result['validFrom_time_t'], result['validTo_time_t']);
+                    progress.show();
 		    $(".table").append($(".entries").get().sort(function(a, b) {
 			var diff = parseInt($(b).find(".progress-bar").attr("aria-valuenow")) - parseInt($(a).find(".progress-bar").attr("aria-valuenow"));
 			return diff ? diff : false;
@@ -60,11 +82,11 @@
     <table class="table table-hover">
       <thead>
         <tr>
-          <th>#</th>
           <th>Fully Qualified Domain Name</th>
           <th>SSL Port</th>
           <th>SSL Certificate Issuer</th>
           <th>♥ Life Spent</th>
+          <th>☠ Time Left</th>
           <th></th>
         </tr>
       </thead>
@@ -76,7 +98,6 @@
 	  $port = $domain_info[1];
 ?>
           <tr class="entries">
-          <th scope="row"><?php echo ++$index ?></th>
           <td class="fqdn"><?php echo $fqdn ?></td>
           <td class="port"><?php echo $port ?></td>
           <td class="issuer"></td>
@@ -86,6 +107,7 @@
                 <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
           </td>
+          <td class="timeleft"></td>
           <td>
               <input class="btn btn-secondary dom-btn" type="button" value="Check">
           </td>
